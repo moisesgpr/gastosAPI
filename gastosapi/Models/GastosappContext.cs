239d366dee32e9d746ce791c8bc09bfdf4883a84
moisesgpr <moisesgp95@gmail.com>
2023-07-06 16:18:15 -0400

    production deploy

--- gastosapi/Models/GastosappContext.cs
+++ gastosapi/Models/GastosappContext.cs
@@ -23,7 +23,9 @@ public partial class GastosappContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=PR\\SQLExpress;Database=gastosapp;Trusted_Connection=True;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("Server=tcp:gastosapp.database.windows.net,1433;Initial Catalog=gastosapp;Persist Security Info=False;User ID=gastosapp;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+       
+       // => optionsBuilder.UseSqlServer("Server=PR\\SQLExpress;Database=gastosapp;Trusted_Connection=True;TrustServerCertificate=true");
         //=> optionsBuilder.UseSqlServer("Server=DESKTOP-SDKDVL8;Database=gastosapp;Trusted_Connection=True;TrustServerCertificate=true");
 
 
